95ab0484ad783cbab016028477f06fe23f84302f
Moritz Hofmann <info@morihofi.de>
2025-06-18 11:40:46 +0200

    build: Add module-info.java for Java 9 and higher

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module de.morihofi.cab4j {
+    requires org.slf4j;
+    requires org.tukaani.xz;
+
+    exports de.morihofi.cab4j;
+    exports de.morihofi.cab4j.file;
+    exports de.morihofi.cab4j.structures;
+    exports de.morihofi.cab4j.util;
+}
